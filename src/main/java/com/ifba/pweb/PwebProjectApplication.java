1ed959979940962041688126ee08d16e9e796853
Matheus Rocha <theucsrocha@gmail.com>
2025-11-05 12:49:23 -0300

    Adiciona arquivos iniciais do projeto PWEB

---
+++ src/main/java/com/ifba/pweb/PwebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.pweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifba/pweb/PwebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba.pweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
